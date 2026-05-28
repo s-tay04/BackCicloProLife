487f783e4eda7d5bd6b27acfbf1f9311c647d6ab
Tayná Leite <tayna.matos@aluno.senai.br>
2026-05-28 15:26:28 -0300

    Data

---
+++ BackCicloProLife/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BackCicloProLife.Models;
+
+namespace BackCicloProLife.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options){ }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Receita> Receita { get; set; }
+        public DbSet<Venda> Venda { get; set; }
+        public DbSet<Ingrediente> Ingrediente { get; set; }
+        public DbSet<IngredienteReceita> IngredienteReceita { get; set; }
+    }
+}
--- BackCicloProLife/Models/Receita.cs
+++ BackCicloProLife/Models/Receita.cs
@@ -1,7 +1,19 @@
-﻿namespace BackCicloProLife.Models
+﻿using System.Security.Principal;
+
+namespace BackCicloProLife.Models
 {
     public class Receita
     {
         public int Id { get; set; }
+        public string Titulo { get; set; }
+        public double Custo { get; set; }
+        public string ModoPreparo { get; set; }
+        public double Porcao { get; set; }
+        public string Status { get; set; }
+        public string FeedbackChefe { get; set; }
+        public int IdUsuarioColaborador { get; set; }
+        public int IdUsuarioChefe { get; set; }
+        public int IdUsuarioGestor { get; set; }
+
     }
 }
